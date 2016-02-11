7a6a97bc79015cac2973745c1c89aa6ae8fdeb29
Luiz Fernando Afra Brito <luizfernandoafrabrito@hotmail.com>
2016-02-11 06:08:41 -0200

    solved some bugs

--- gifit.sh
+++ gifit.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-PNG_DIR=$(mktemp -d)
 DELAYS=()
 MODE=0
 SPEED_FACTOR=1
 SCALE_FACTOR=1
-SLEEP_TIME=0.0666
+SLEEP_TIME=0.1
 WINDOW_GEOMETRY=0
-OPTIONAL_TMP_DIR=$(printenv GIFIT_TMP_DIR)
-OPTIONAL_GIF_DIR=$(printenv GIFIT_GIF_DIR)
+TMP_DIR=$([ $GIFIT_TMP_DIR ] && echo $(mktemp -d -p $GIFIT_TMP_DIR) || echo $(mktemp -d))
+GIF_DIR=$([ $GIFIT_GIF_DIR ] && echo $GIFIT_GIF_DIR || echo ".")
+
 while getopts ":n:v:t:ws" opt; do
   case $opt in
     n)
@@ -61,15 +61,26 @@ do_shots(){
   count=0
   keypress=''
   last_date=$(date +%s%2N)
+
   while [[ "$keypress" != "q" ]]; do
-    maim -g $1 "$PNG_DIR/$(printf %05d $count).png"
-    current_date=$(date +%s%2N)
-    DELAYS[$count]=$(($current_date-$last_date))
-    last_date=$current_date
+    scrot "$TMP_DIR/$(printf %05d $count).pnm"
 
     echo -ne "Press [q] to stop recording\ttotal shots = $(($count+1))\r"
 
-    sleep $SLEEP_TIME
+    current_date=$(date +%s%2N)
+    DELAYS[$count]=$(($current_date-$last_date))
+    real_sleep=$(echo "scale=2;($SLEEP_TIME-(${DELAYS[$count]}/100))" | bc)
+    echo "count = $count, sleep = $SLEEP_TIME, sleeped = ${DELAYS[$count]}, real = $real_sleep" >> log
+
+    if [ $(echo "$real_sleep > 0" | bc) -eq 1 ]; then
+        echo "entered" >> log
+        sleep $real_sleep
+    else
+        echo "not entered" >> log
+    fi
+
+    last_date=$(date +%s%2N)
+
     let count+=1
     keypress="`cat -v`"
   done
@@ -79,9 +90,8 @@ do_shots(){
 }
 
 scale_shots(){
-    echo "Scaling shots"
-
-    mogrify -scale "$(echo "$SCALE_FACTOR*100"| bc)%" $PNG_DIR/*
+    echo "Scaling..."
+    gm mogrify -scale "$(echo "$SCALE_FACTOR*100"| bc)%" "$TMP_DIR/*"
 }
 
 get_index_from_file()
@@ -95,59 +105,39 @@ get_index_from_file()
 
 remove_shots()
 {
-    files=$(yad  --title="Select a file to remove" --add-preview --multiple --image-filter --splash --filename="$PNG_DIR/" --file-selection)
+    files=$(yad  --title="Select a file to remove" --add-preview --multiple --image-filter --splash --filename="$TMP_DIR/" --file-selection)
 
     IFS='|' read -ra IMGS <<< "$files"
 
     for img in "${IMGS[@]}"; do
-        DELAYS[$(get_index_from_file $img)]=-1
         rm $img
     done
 }
 
 shots_to_gif(){
-  _CONVERT="convert -limit memory 2GiB -limit map 4GiB "
 
-  if [ -n "$OPTIONAL_TMP_DIR" ]; then
-    _CONVERT="${_CONVERT} -define registry:temporary-path=$OPTIONAL_TMP_DIR "
-  fi
-
-
-  for f in $PNG_DIR/*; do
-      index=$(get_index_from_file $f)
-
-      if [[ ${DELAYS[$index]} -ne -1 ]]; then
-        delay=$(echo "${DELAYS[$index]}/$SPEED_FACTOR" | bc -l)
-        _CONVERT="${_CONVERT} -delay $delay $f "
-      fi
-  done
-
-  new_gif=$PNG_DIR/out.gif
-  _CONVERT="${_CONVERT} -loop 0 -fuzz 2% -layers Optimize  $new_gif"
   echo "Making gif..."
 
-  eval $_CONVERT
+  if [ $MODE -ne 0 ]; then
+    gm mogrify -crop $WINDOW_GEOMETRY\! $TMP_DIR/*.pnm
+  fi
+
+  gm convert +dither -colorspace YUV -colors 63 -fuzz 10% -delay $(echo "100*$SLEEP_TIME/$SPEED_FACTOR" | bc -l) $TMP_DIR/*.pnm $TMP_DIR/out.gif
 
   if [ "$?" -ne "0" ]; then
     echo "Gif failed. Try to set the variable GIFIT_TMP_DIR in your ~/.bashrc. Choose a temporary folder with alot of space!"
     exit 1
   fi
 
-
-  if [ -n "$OPTIONAL_GIF_DIR" ]; then
-    optimized_gif="$OPTIONAL_GIF_DIR/$(date +%F-%T).gif"
-  else
-    optimized_gif="$(date +%F-%T).gif"
-  fi
-
-  gifsicle -O2 $new_gif -o $optimized_gif
+  echo "Optimizing..."
+  gifsicle -O3 --method blend-diversity --colors 63  $TMP_DIR/out.gif -o $GIF_DIR/$(date +%F-%T).gif
 
   echo "Gif OK!"
 }
 
 get_window_geometry
 paplay ./race-countdown.ogg
-do_shots $WINDOW_GEOMETRY
+do_shots
 
 echo "Do you wanna remove some shots? (Y/n)"
 read remove
@@ -162,6 +152,6 @@ fi
 
 shots_to_gif
 
-rm -rf $PNG_DIR
+rm -rf $TMP_DIR
 
 echo "Thanks for using this script."
